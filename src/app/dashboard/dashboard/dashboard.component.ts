b228556f10b7138bb0c5d00b69dcf4154afe9f23
Theophilus Omorebee <theo4u@ymail.com>
2017-07-14 19:10:17 +0100

    added logout functionality

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../providers/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +9,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {
+  constructor(private _authService: AuthService, private _router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  /**
+   * logout our user from the system
+   */
+  logout(): void {
+    this._authService.logout()
+      .subscribe((message: string) => {
+        //navigate to login page
+        this._router.navigate(['/login']);
+      }, (error) => {
+        console.error(error);
+        this._authService.clear();//incase the network had issues
+        this._router.navigate(['/login']);
+      });
   }
 
 }
